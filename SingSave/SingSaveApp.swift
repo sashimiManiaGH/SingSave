28af1e3797648a8b7f66bbc04df94cd1e9ec4f16
Leung Yin <leungyinchiew@gmail.com>
2024-09-13 18:54:48 +0800

    Initial Commit

---
+++ SingSave/SingSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingSaveApp.swift
+//  SingSave
+//
+//  Created by Leung Yin on 13/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SingSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
