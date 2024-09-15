3769bc4821c7442e7cef9b14584a90f9dab8b3f8
Leung Yin <leungyinchiew@gmail.com>
2024-09-15 15:40:19 +0800

    Commit: Changed the change of goals and budget to keypad

--- SingSave/UserChangeView_Budget.swift
+++ SingSave/UserChangeView_Budget.swift
@@ -10,33 +10,20 @@ import SwiftUI
 struct UserChangeView_Budget: View {
     
     @State private var changeBudget = ""
-    @State private var changeBudget_noLetters = false
     
     @Binding var monthlyUseGoal: Double
     @Binding var saveGoals: Double
     
     @Environment(\.dismiss) var dismiss
     
-    func checkDouble(value: String, binding: Bool) {
-        let allowedCharacters = CharacterSet.alphanumerics.union(CharacterSet(charactersIn: "."))
-        let doesNotContainForbiddenCharacters = value.rangeOfCharacter(from: allowedCharacters.inverted) == nil
-        if doesNotContainForbiddenCharacters == true {
-            changeBudget_noLetters = true
-        } else {
-            changeBudget = "Invalid Input"
-        }
-    }
-    
     var body: some View {
         List {
             Text("Change Your Budget:")
                 .bold()
             TextField("Type a valid number", text: $changeBudget)
+                .keyboardType(.decimalPad)
             Button("Change Budget") {
-                checkDouble(value: changeBudget, binding: changeBudget_noLetters)
-                if changeBudget_noLetters == true {
-                    monthlyUseGoal = Double(changeBudget)!
-                }
+                monthlyUseGoal = Double(changeBudget) ?? 0
                 dismiss()
             }
         }
@@ -46,33 +33,20 @@ struct UserChangeView_Budget: View {
 struct UserChangeView_Save: View {
     
     @State private var changeSave = ""
-    @State private var changeSave_noLetters = false
     
     @Binding var monthlyUseGoal: Double
     @Binding var saveGoals: Double
     
     @Environment(\.dismiss) var dismiss
     
-    func checkDouble(value: String, binding: Bool) {
-        let allowedCharacters = CharacterSet.alphanumerics.union(CharacterSet(charactersIn: "."))
-        let doesNotContainForbiddenCharacters = value.rangeOfCharacter(from: allowedCharacters.inverted) == nil
-        if doesNotContainForbiddenCharacters == true {
-            changeSave_noLetters = true
-        } else {
-            changeSave = "Invalid Input"
-        }
-    }
-    
     var body: some View {
         List {
             Text("Change Your Budget:")
                 .bold()
             TextField("Type a valid number", text: $changeSave)
+                .keyboardType(.decimalPad)
             Button("Change Budget") {
-                checkDouble(value: changeSave, binding: changeSave_noLetters)
-                if changeSave_noLetters == true {
-                    monthlyUseGoal = Double(changeSave)!
-                }
+                monthlyUseGoal = Double(changeSave) ?? 0
                 dismiss()
             }
         }
